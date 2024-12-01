c797e0b1b074d4d73031731393f67f54953a747e
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-12-01 16:26:42 +0700

    halaman home

---
+++ perpustakaan-app/app_server/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+// import controller
+const mainController = require('../controllers/mainController');
+
+//route
+router.get('/', mainController.use);
+
+router.get('/home', mainController.index);
+
+// router.get('/about', mainController.about);
+
+// router.get('/contact', mainController.contact);
+
+
+module.exports = router;
