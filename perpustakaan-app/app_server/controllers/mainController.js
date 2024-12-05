4827309fe9c1d366776e74b87478f06332273676
OvanK <ovankurniawan04@gmail.com>
2024-12-05 13:24:56 +0700

    Pembuatan About dan fix lainnya
    Membuat halaman about serta memperbaiki typo yang ada di title "Perpustakaan Digital ENOJ"
    
    Membuat ketika title "Perpustakaan Digital ENOJ" ditekan maka akan kembali ke home.
    
    Menata ulang beberapa text serta penghapusan scrollbar pada menu home agar lebih astetik rawr

--- perpustakaan-app/app_server/controllers/mainController.js
+++ perpustakaan-app/app_server/controllers/mainController.js
@@ -1,34 +1,40 @@
 const index = (req, res) => {
-    const perpus =[
-        {
-            judul: "Pemrograman Aplikasi Web 1",
-            isi: "berisi tentang pembuatan aplikasi berbasis web"
-        },
-        {
-            judul: "Pemrograman Aplikasi Web 2",
-            isi: "berisi tentang pembuatan aplikasi berbasis web"
-        },
-        {
-            judul: "Pemrograman Aplikasi Bergerak 1",
-            isi: "berisi tentang pembuatan aplikasi berbasis mobile"
-        },
-        {
-            judul: "Pemrograman Aplikasi Bergerak 2",
-            isi: "berisi tentang pembuatan aplikasi berbasis mobile"
-        },
-        {
-            judul: "Peracangan Aplikasi",
-            isi: "berisi tentang bagaimana merancang sebuah aplikasi"
-        },
-        {
-            judul: "Analisis",
-            isi: "berisi tentang bagaimana mencari sebuah ide"
-        },
-    ];
-    res.render('home',{title: 'halaman home',perpus, layout:'main'});
-}
+  const perpus = [
+    {
+      judul: "Pemrograman Aplikasi Web 1",
+      isi: "berisi tentang pembuatan aplikasi berbasis web",
+    },
+    {
+      judul: "Pemrograman Aplikasi Web 2",
+      isi: "berisi tentang pembuatan aplikasi berbasis web",
+    },
+    {
+      judul: "Pemrograman Aplikasi Bergerak 1",
+      isi: "berisi tentang pembuatan aplikasi berbasis mobile",
+    },
+    {
+      judul: "Pemrograman Aplikasi Bergerak 2",
+      isi: "berisi tentang pembuatan aplikasi berbasis mobile",
+    },
+    {
+      judul: "Peracangan Aplikasi",
+      isi: "berisi tentang bagaimana merancang sebuah aplikasi",
+    },
+    {
+      judul: "Analisis",
+      isi: "berisi tentang bagaimana mencari sebuah ide",
+    },
+  ];
+  res.render("home", { title: "halaman home", perpus, layout: "main" });
+};
 
-const use = ("/",(req, res)=>{
+const about = (req, res) => {
+  res.render("about", { title: "About", layout: "main" });
+};
+
+const use =
+  ("/",
+  (req, res) => {
     res.send("<h1>404 Not Found</h1>");
-});
-module.exports = {index, use}
+  });
+module.exports = { index, use, about };
--- perpustakaan-app/app_server/routes/index.js
+++ perpustakaan-app/app_server/routes/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // import controller
-const mainController = require('../controllers/mainController');
+const mainController = require("../controllers/mainController");
 
 //route
-router.get('/', mainController.use);
+router.get("/", mainController.use);
 
-router.get('/home', mainController.index);
+router.get("/home", mainController.index);
 
 // router.get('/about', mainController.about);
+router.get("/about", mainController.about);
 
 // router.get('/contact', mainController.contact);
-
 
 module.exports = router;
