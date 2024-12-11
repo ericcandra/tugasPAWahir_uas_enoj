4137abd042baf562e13f2fd75f320d520a556870
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-12-11 19:18:45 +0700

    mongdb cloud

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         //menghubungkan ke mongodb menggunakan url koneksi
         await mongoose.connect(
             // "mongodb+srv://ericcandra:eric123@cluster0.hadmb.mongodb.net/mdpdb?retryWrites=true&w=majority&appName=Cluster0"
-            "mongodb+srv://perpustakaan:12345@cluster0.hadmb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://perpustakaan:12345@cluster0.hadmb.mongodb.net/perpustakaan?retryWrites=true&w=majority&appName=Cluster0"
         );
         // jika koneksi berhasil log pesan ke konsol
         console.log("MongoDb Connected");
