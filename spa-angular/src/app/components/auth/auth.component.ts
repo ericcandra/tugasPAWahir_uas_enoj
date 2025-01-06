c45e2fbf83581ac66d548a10872871acf15bbd15
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2025-01-06 17:28:06 +0700

    asa

--- spa-angular/src/app/components/auth/auth.component.ts
+++ spa-angular/src/app/components/auth/auth.component.ts
@@ -64,7 +64,7 @@ export class AuthComponent {
           console.log('Login berhasil:', response);
 
           // Mengarahkan ke halaman fakultas setelah login berhasil
-          window.location.href = '/';  // Menggunakan window.location.href untuk mereload halaman dan mengarahkan ke /fakultas
+          window.location.href = '/buku';  // Menggunakan window.location.href untuk mereload halaman dan mengarahkan ke /fakultas
           // this.router.navigate(['/fakultas']);  // Alternatif menggunakan Angular router (jika tidak menggunakan window.location.href)
         } else {
           // Jika tidak ada token dalam response, tampilkan pesan error
